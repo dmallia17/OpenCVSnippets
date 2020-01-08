f4855f1f9c94655e12a4b5a4f730844958e680f8
dmallia17 <Daniel.Mallia21@myhunter.cuny.edu>
2020-01-08 18:53:21 -0500

    Initial commit

---
+++ scannerv1.py
@@ -0,0 +1,18 @@
+#! /usr/bin/env python3
+
+# Author: Daniel Mallia
+
+# This short program should perform the basic function of a document scanner "app":
+# 1. Retrieve an image
+# 2. Perform edge detection
+# 3. Perform a perspective transformation for a better image for OCR
+# 4. Sharpen the image.
+
+import cv2 as cv
+import sys
+
+def transform():
+
+
+if __name__ == "__main__":
+
