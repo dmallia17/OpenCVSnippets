6c53cfa563627f7b6eab2f3f4d5fbdf46cd36451
dmallia17 <Daniel.Mallia21@myhunter.cuny.edu>
2020-01-09 10:32:50 -0500

    Add comments and remove some hardcoding

--- scannerv1.py
+++ scannerv1.py
@@ -14,28 +14,38 @@ import sys
 import copy
 import numpy as np
 
+# Helper function - returns area for list sorting
 def sortByArea(contour):
     return cv.contourArea(contour)
 
+# Transformation function - "snaps" document to frame.
 def transform(document):
+    rows, cols, _ = document.shape
+    max_row = rows - 1
+    max_col = cols - 1
     documentCopy = copy.deepcopy(document)
-    documentCopy = cv.cvtColor(documentCopy, cv.COLOR_RGB2GRAY)
-    _, documentCopy = cv.threshold(documentCopy, 180, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+    documentCopy = cv.cvtColor(documentCopy, cv.COLOR_RGB2GRAY) # Convert to grayscale
+
+    # Apply Otsu binary thresholding
+    _, documentCopy = cv.threshold(documentCopy, 180, 255, cv.THRESH_BINARY+cv.THRESH_OTSU) 
+
+    # Find contours and select the contour which covers the most area - should be the desired document
     contours, hierarchy = cv.findContours(documentCopy, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
-    contours.sort(key = sortByArea)
-    #cv.drawContours(document, contours, (len(contours) - 1), (0, 0, 255), 10)
-    possibleDocument = contours[-1]
+    contours.sort(key = sortByArea, reverse=True) 
+    #cv.drawContours(document, contours, 0, (0, 0, 255), 10) # Draw greatest area contour for debugging
+    possibleDocument = contours[0]
 
     epsilon = 0.1*cv.arcLength(possibleDocument,True)
-    approx = cv.approxPolyDP(possibleDocument,epsilon,True)
-    print(approx)
+    approx = cv.approxPolyDP(possibleDocument,epsilon,True) # approximate contour with rectangle
 
-    dest = np.float32([[0,0],[4031,0], [0,3023], [4031, 3023]])
+    dest = np.float32([[0,0],[max_row,0], [0,max_col], [max_row, max_col]])
+    # Fix the following hardcoding:
     matrix = cv.getPerspectiveTransform(np.float32([approx[0], approx[3], approx[1], approx[2]]), dest)
     
-    document = cv.warpPerspective(document, matrix, (4031, 3023))
+    # Apply perspective transformation
+    transformedDocument = cv.warpPerspective(document, matrix, (max_row, max_col))
 
-    return document
+    return transformedDocument
 
 if __name__ == "__main__":
     # Usage:
