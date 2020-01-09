e4441b227631d7989a23d002f23b1dba5b18bd60
dmallia17 <Daniel.Mallia21@myhunter.cuny.edu>
2020-01-08 20:30:00 -0500

    Progress on document detection

--- scannerv1.py
+++ scannerv1.py
@@ -7,12 +7,42 @@
 # 2. Perform edge detection
 # 3. Perform a perspective transformation for a better image for OCR
 # 4. Sharpen the image.
+# 5. Save the image.
 
 import cv2 as cv
 import sys
+import copy
 
-def transform():
+def sortByArea(contour):
+    return cv.contourArea(contour)
 
+def transform(document):
+    documentCopy = copy.deepcopy(document)
+    documentCopy = cv.cvtColor(documentCopy, cv.COLOR_RGB2GRAY)
+    _, documentCopy = cv.threshold(documentCopy, 180, 255, cv.THRESH_BINARY+cv.THRESH_OTSU)
+    contours, hierarchy = cv.findContours(documentCopy, cv.RETR_TREE, cv.CHAIN_APPROX_NONE)
+    contours.sort(key = sortByArea)
+    
+    cv.drawContours(document, contours, (len(contours) - 1), (0, 0, 255), 10)
+    return document
 
 if __name__ == "__main__":
+    # Usage:
+    if(len(sys.argv) != 2):
+        print('Usage: ./scannerv1.py [IMAGE]')
+        sys.exit()
 
+    imageName = sys.argv[1]
+    document = cv.imread(imageName, cv.IMREAD_COLOR)
+    
+    document = transform(document)
+
+    cv.namedWindow('final', cv.WINDOW_NORMAL)
+    cv.resizeWindow('final', 640, 480)
+    
+    # Display
+    cv.imshow('final', document)
+    cv.waitKey(0)
+    cv.destroyWindow('final')
+
+    # Save the image
